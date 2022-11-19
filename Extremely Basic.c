b70ea06055df9d34580e553a62704e94b15a0843
Meher Afroz Antor <meherafrozantor@gmail.com>
2022-11-19 20:08:53 +0600

    Create Extremely Basic.c

---
+++ Extremely Basic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A, B, sum;
+    
+    scanf("%d\n", &A);
+    scanf("%d\n", &B);
+    
+    sum = A + B;
+    
+    printf("X = %d\n", sum);
+    
+ 
+    return 0;
+}
